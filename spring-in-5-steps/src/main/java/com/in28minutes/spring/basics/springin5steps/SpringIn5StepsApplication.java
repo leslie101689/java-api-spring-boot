65be982b84c0893972f667de31f7d0955448a7ef
leslie101689 <49891074+leslie101689@users.noreply.github.com>
2020-04-21 09:35:31 +0800

    Step 4 _ Using Spring to Manage Dependencies - @Component_ @Autowired

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,19 +2,24 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
+		//What are the beans? @Component
+		//What are the dependencies of the bean? @Autowired
+		//Where to search for beans? @SpringBootApplication
 	
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		
+		//Application Context
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
 		System.out.println(result);
-		//com.in28minutes.spring.basics.springin5steps.BubbleSortAlgorithm@41a4555e
-		//com.in28minutes.spring.basics.springin5steps.QuickSortAlgorithm@41a4555e
 		
-		//SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 
 }
