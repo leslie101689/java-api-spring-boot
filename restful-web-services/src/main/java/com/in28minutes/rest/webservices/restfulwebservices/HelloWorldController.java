f9777f25144e37eceea135d1e0828e72b4fac7cc
leslie101689 <49891074+leslie101689@users.noreply.github.com>
2020-05-03 08:25:37 +0800

    Step 04 - Enhancing the Hello World Service to return a Bean

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -8,13 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller
 @RestController
 public class HelloWorldController {
-	//GET
-	//URI - /hello-world
-	//method = "Hello World"
-//	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	//hello-world-bean
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 
 }
