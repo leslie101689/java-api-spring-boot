83c481d438a2904e5d28f2900d5de8f125c6837e
leslie101689 <49891074+leslie101689@users.noreply.github.com>
2020-05-13 15:12:12 +0800

    Step 22 - Enhancing Swagger Documentation with Custom Annotations

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,8 @@ import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +24,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	public static final Contact DEFAULT_CONTACT = new Contact(
+			"Leslie", "http://www.in28minutes.com", "in28minutes@gmail.com");
+	
+	  public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+			  "Api Documentation", "Awesome Api Description", "1.0", 
+			  "urn:tos",
+	          DEFAULT_CONTACT, 
+	          "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = 
+			new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
 	@Primary
 	@Bean
 	public LinkDiscoverers discoverers() {
@@ -29,7 +46,10 @@ public class SwaggerConfig {
 	
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 	//Swagger 2
 	//All the paths
