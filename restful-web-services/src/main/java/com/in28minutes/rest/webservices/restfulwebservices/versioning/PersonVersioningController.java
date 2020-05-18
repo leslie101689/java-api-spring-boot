376503c4d6dfec174239e4e7d859eaeb010b6830
leslie101689 <49891074+leslie101689@users.noreply.github.com>
2020-05-19 07:40:11 +0800

    Step 26 - Versioning RESTful Services - Basic Approach with URIs

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -15,5 +15,41 @@ public class PersonVersioningController {
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+//	/person/param?version=1
+	@GetMapping(value="/person/param", params="version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+//	/person/param?version=2
+	@GetMapping(value="/person/param", params="version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
 
+//	/person/header + X-API-VERSION(Header)
+	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+//	/person/header + X-API-VERSION(Header)
+	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+//	/person/produces + Accept(Header)
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+//	/person/produces + Accept(Header)
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
 }
